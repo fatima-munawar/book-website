d618ae550aaf91fe2d19cd5fe3ac417cd89f2124
fatima-munawar <fatimamunawar124@gmail.com>
2026-01-30 03:33:51 -0700

    Add initial files

---
+++ script.js
@@ -0,0 +1,168 @@
+/*=============== SEARCH ===============*/
+const searchButton = document.querySelector("#search-button");
+const searchClose = document.querySelector("#search-close");
+const searchContent = document.querySelector("#search-content");
+if(searchButton){
+    searchButton.addEventListener("click", () =>{
+        searchContent.classList.add('show-search')
+    })
+}
+if(searchClose){
+    searchClose.addEventListener("click", () =>{
+        searchContent.classList.remove('show-search')
+    })
+}
+
+/*=============== LOGIN ===============*/
+const loginButton = document.querySelector("#login-button");
+const loginClose = document.querySelector("#login-close");
+const loginContent = document.querySelector("#login-content");
+if(loginButton){
+    loginButton.addEventListener("click", () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+if(loginClose){
+    loginClose.addEventListener("click", () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                        :header.classList.remove('shadow-header');
+}
+window.addEventListener("scroll", shadowHeader)
+
+/*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home--swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: 'false',
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+})
+
+/*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper('.featured--swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+    breakpoints: {
+        1150: {
+            slidesPerView: 4,
+            centeredSlides: false,
+        }
+    }
+})
+
+/*=============== NEW SWIPER ===============*/
+let swiperNew = new Swiper('.new--swiper', {
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+
+    breakpoints: {
+        1150: {
+            slidesPerView: 3,
+        }
+    }
+})
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper('.testimonial--swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    breakpoints: {
+        1150: {
+            slidesPerView: 3,
+            centeredSlides: false,
+        }
+    }
+})
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollup = () => {
+    const scrollup = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll')
+                        :scrollup.classList.remove('show-scroll');
+}
+window.addEventListener("scroll", scrollup)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    sections.forEach(current => {
+        const sectionHeight = current.offetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = getAttribute(''),
+        sectionsClass = document.querySelector('nav-menu a[href*= ' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }
+        else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const currentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const currentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme) 
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme) 
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', currentTheme())
+    localStorage.setItem('selected-icon', currentIcon())
+})
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: 'true',
+})
+sr.reveal(`.home--data, .featured--container, .new--container, .join--data, .testimonial--container, .footer`)
+sr.reveal(`.home--images`, {delay: 600})
+sr.reveal(`.services--card`, {interval: 100})
+sr.reveal(`.discount--data`, {origin: 'left'})
+sr.reveal(`.discount--description`, {origin: 'right'})
